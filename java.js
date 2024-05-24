f66a8076fb817342930c78dc5df009565e2db8b0
Abdelkrim <108987751+AbdelkrimBelmokhtar@users.noreply.github.com>
2024-05-24 11:55:44 +0200

    Add files via upload

---
+++ java.js
@@ -0,0 +1,8 @@
+document.getElementById('paymentForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Empêcher la soumission du formulaire pour la démo
+
+    // Ici, vous ajouterez la validation du formulaire
+    // et la logique d'intégration avec le système de paiement
+
+    alert('Paiement simulé pour la démonstration');
+});
